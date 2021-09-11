e94ec9a7b1c24cf639083b9ee5c2587104ae6888
tian951377408 <951377408@qq.com>
2021-09-11 21:57:48 +0800

    ddd

---
+++ src/test/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @Project: idea_maven_code
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: java32期 T0123
+ * @Author: tian
+ * @Create: 2021-09-11 21:52
+ * @Desc：
+ **/
+public class Hello {
+    public static void main ( String[] args ) {
+        System.out.println("XXX");
+    }
+}
